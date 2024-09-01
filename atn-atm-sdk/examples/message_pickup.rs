2c817049c3093bbe9f8f79bb721c518d61ac4e0e
Glenn Gore <glenn.g@affinidi.com>
2024-09-02 07:28:15 +0800

    Fixing compile issues with message_pickup example

--- atn-atm-sdk/examples/message_pickup.rs
+++ atn-atm-sdk/examples/message_pickup.rs
@@ -1,13 +1,11 @@
 use std::time::Duration;
 
 use atn_atm_sdk::{
-    config::Config, conversions::secret_from_str, errors::ATMError, protocols::Protocols,
-    transports::SendMessageResponse, ATM,
+    config::Config, conversions::secret_from_str, errors::ATMError, protocols::Protocols, ATM,
 };
-use did_peer::DIDPeer;
-use http::response;
+
 use serde_json::json;
-use tracing::{debug, error, info, warn};
+use tracing::{error, info};
 use tracing_subscriber::filter;
 
 #[tokio::main]
@@ -51,10 +49,8 @@ async fn main() -> Result<(), ATMError> {
         .build()?;
 
     // Create a new ATM Client
-    let mut atm = ATM::new(config, vec![Box::new(DIDPeer)]).await?;
-
-    // Going to work with higher level DIDComm protocols
-    let mut protocols = Protocols::new(&mut atm);
+    let mut atm = ATM::new(config).await?;
+    let protocols = Protocols::new();
 
     /*let message = r#"
     {"protected":"eyJ0eXAiOiJhcHBsaWNhdGlvbi9kaWRjb21tLWVuY3J5cHRlZCtqc29uIiwiYWxnIjoiRUNESC0xUFUrQTI1NktXIiwiZW5jIjoiQTI1NkNCQy1IUzUxMiIsInNraWQiOiJkaWQ6cGVlcjoyLlZ6Nk1rZ1dKZlZtUEVMb3pxNmFDeWNLM0NweEhOOFVwcGhuM1dTdVFrV1k2aXFzakYuRXpRM3NoZmI3dndRYVRKcUZrdDhuUmZvN051OTh0bWVZcGREZldncnFRaXREYXFYUnoja2V5LTIiLCJhcHUiOiJaR2xrT25CbFpYSTZNaTVXZWpaTmEyZFhTbVpXYlZCRlRHOTZjVFpoUTNsalN6TkRjSGhJVGpoVmNIQm9iak5YVTNWUmExZFpObWx4YzJwR0xrVjZVVE56YUdaaU4zWjNVV0ZVU25GR2EzUTRibEptYnpkT2RUazRkRzFsV1hCa1JHWlhaM0p4VVdsMFJHRnhXRko2STJ0bGVTMHkiLCJhcHYiOiI1a05fc2kyd2toMFVQX0ZlNVNLejVLQkJPNkYzMVRneXJBNEZ5Z1hTeExZIiwiZXBrIjp7ImNydiI6InNlY3AyNTZrMSIsImt0eSI6IkVDIiwieCI6IlluMllkX1BVUUZTSVQtaWt1WlFSZ0kyVmgzdFFOVXVLdWtEdWZ1clpHMzQiLCJ5IjoidkpRV1Y3U09jUWNtMkNwUkI1S3ZDMU5YeUxrS1hmdndwdVdlQlZ2RkY0RSJ9fQ","recipients":[{"header":{"kid":"did:peer:2.Vz6MkiToqovww7vYtxm1xNM15u9JzqzUFZ1k7s7MazYJUyAxv.EzQ3shQLqRUza6AMJFbPuMdvFRFWm1wKviQRnQSC1fScovJN4s.SeyJ0IjoiRElEQ29tbU1lc3NhZ2luZyIsInMiOnsidXJpIjoiaHR0cHM6Ly8xMjcuMC4wLjE6NzAzNyIsImEiOlsiZGlkY29tbS92MiJdLCJyIjpbXX19#key-2"},"encrypted_key":"_tK6Tu4uzgmKqJATtvQetmPSYC1dGKnLfDUOc6XLr4LDJHT1ujrAPtm-p2pIUd1Pewrt35CR0hK3-rdK0ffGSLJrP1ntrJ1K"}],"iv":"HP4BfkLKN5X0WTbu-PMV5g","ciphertext":"hQ4TLBC-yjGFx6GZonfzSM5DB8W9rNYeIW0eAPf1vxAkj0zuXkwsLx9tEyTbS1Oc46DDaIuMzFYcbfAyrioUNOUp-43OHGxtVKXues4wTNvMFVbWEkOq27bm18APj59JohT5QAd8Dn-iZ1vPl82MZbwxpymPWS0MQqdGB_OoY1Clm6nE7lDM3plgxbvH0VnUWopCrHHHhKE00esX7Ax5fO8RJLmdSt8DMPrVII3qOASDJIBHc2f5af06o7PytErkoN_d8oOyl2QhoQm7HWe1gWFXZcxxeZaFRqdJDcVrUJ_h3X7S21qxn38TdZ7r5fgGu9as0GvzkHCZbn9OBCjpQ6cUQNdB6NRuUXhjgWFH1vXALAiRn3OLsgiUVJM__Nr3EWLOgOeBLDz76K6LuXfFZApYjErf6doLbjMQ_vlb93r9-HNKy5xNHKZJF0i7qPCkZJUOPxm7DUsYt_zmq3gNs9C6DAug8Dp5ayBPriNUxgWBXpwaJyFB4idyMDs3rNyvsswm_Fz1ImiHT4w0DvCDPvgBbGgEot0wqzKdT-cjNAHaXIF_O-t1pzanrjUFU16VVeuyZ0m0WWB0qRnZWpADpWy_9Vdpjvvh3kw8BO05QwvTzdR82ed1R4JX2HHTZR3GNjvHy-y_b4sXK0yKAFrrSULcwLfJY-8tTLrfIlSssT9LMmBLJKBlhdIiG0gGyw0_Zf_3PocQ8a-MRcqdMFguKPAX49PcMnaSRkQkAPPYTPWp0zv8p_HREY3h8WIYWYhrfD_wAeJSAlaISxH-7RJkdnweauKyZ5sIQ4HZ7oqfw3ARR5IaJxI9N1H7gKuphLe27oYhHqbPb5OJmqlCOFi2MwmFruW9hZMitG9nYkr1GNF4rxfoT7kOfsLoEAkxmNHrBWqSoGdsCOtYCAbjeWNf6mtPoCaTfELf0hgzkOD5UXfgNO6oKoB76Y6YsuUNPbULPkEu20TDz9b2MFGkfyXONHNgA46MErNaWFM24J4-yK2hJ7YaiQ7i4x1ZvUTayoF4Co-1a4IS3Tc4kx1HF11vPzWRLQGgXtAh0LQKaoRhspMPUA2MSwWBu9Pf8cKBrY99NuQ1qVZaz8JmzikIDXEuLh6wPmny1OlvyWC4BvqPVX4-IwQV5c7Y_UiXs4p9_OU0Ioq-eA2SaZQRt1IXAodIZJ0mOf3L5om_ngoazEU9cc3N2BdwlrQ9i0fGtRmxC6nk03HqeFV182ZM8x8NFOIzRx_5lYBuTZVgjzV6vVVYK2LI_1hmGFkm5pr77vLidbSxH_oi7INANDcV9PzLKY5NB-1rdXvSUSkw5kQ2W_50HTTzLtluDD91q4DtJZg9rPI_7YFJUxWhJHOX5-iuitZNu788Cng1iHvKV2ylqIJ8GHp3f_FrIgfe8KFeULiuqQWbDgjp71NWPnk_GyNKWcGrcGx03sRV2U1l3kptkZ_Nv8yjsZygfW2iYM9vm_X-9f2rufPlpL2BmnzrmXpOBGIkk2TSXCYY5TzO65igG3yAE7-hrg02vIwbi7msUCMtiHcY","tag":"Tr-FOMqIRytEJvm1KSLFhalhErUUKZbIOWvJgyAaV2M"}
@@ -67,23 +63,19 @@ async fn main() -> Result<(), ATMError> {
     //atm.close_websocket().await?;
 
     // Enable live streaming
-    protocols.message_pickup.toggle_live_delivery(true).await?;
-
-    // Send a Message Pickup 3.0 Status Request
-    /*error!("Testing live_stream_next()!");
-    let response = protocols
+    protocols
         .message_pickup
-        .send_status_request(&mut atm, None, None)
+        .toggle_live_delivery(&mut atm, true)
         .await?;
 
-    // Check if we received a status
-    if let SendMessageResponse::RestAPI(Some(status)) = response {
-        info!("Status: {:?}", status);
-    } else if let SendMessageResponse::WebSocket(response) = response {
-        info!("WebSocket Status: {:?}", response);
-    } else {
-        warn!("No status received");
-    }
+    // Send a Message Pickup 3.0 Status Request
+    error!("Testing live_stream_next()!");
+    let status = protocols
+        .message_pickup
+        .send_status_request(&mut atm, None, None, None)
+        .await?;
+
+    info!("Status: {:?}", status);
 
     if let Some((message, _)) = protocols
         .message_pickup
@@ -91,12 +83,12 @@ async fn main() -> Result<(), ATMError> {
         .await?
     {
         info!("Message: {:?}", message);
-    }*/
+    }
 
     error!("Testing delivery-request()!");
     let response = protocols
         .message_pickup
-        .send_delivery_request(None, None, None, None)
+        .send_delivery_request(&mut atm, None, None, None, None)
         .await?;
 
     let mut delete_ids: Vec<String> = Vec::new();
@@ -108,39 +100,33 @@ async fn main() -> Result<(), ATMError> {
 
     let response = protocols
         .message_pickup
-        .send_messages_received(None, None, &delete_ids, None)
+        .send_messages_received(&mut atm, None, None, &delete_ids, None)
         .await?;
 
-    /*
+    info!("Status: after send_messages_received() : {:?}", response);
+
+    /* TODO: Need to complete this part of the protocol...
+
     tokio::time::sleep(Duration::from_secs(1)).await;
     error!("Testing live_stream_get()!");
 
     let response = protocols
         .message_pickup
-        .send_status_request(&mut atm, None, None)
+        .send_status_request(&mut atm, None, None, None)
         .await?;
 
-    if let SendMessageResponse::WebSocket(response) = response {
-        debug!("Trying to get msg-id ({})", response.message_id);
-        if let Some((message, _)) = protocols
-            .message_pickup
-            .live_stream_get(&mut atm, &response.message_id, Duration::from_secs(2))
-            .await?
-        {
-            info!("Message: {:?}", message);
-        }
-    }
+    info!("Status: {:?}", response);
     */
 
     // Disable live streaming
-    /*protocols
+    protocols
         .message_pickup
         .toggle_live_delivery(&mut atm, false)
         .await?;
 
-    tokio::time::sleep(Duration::from_secs(1)).await;*/
+    tokio::time::sleep(Duration::from_secs(1)).await;
 
-    //atm.abort_websocket_task().await?;
+    atm.abort_websocket_task().await?;
 
     Ok(())
 }
