a281dc3c002229b5051fe05eba1d50a7a033a35f
Glenn Gore <glenn.gore@gmail.com>
2024-09-13 18:28:25 +0800

    feat: removing the example WASM implementation under affinidi-messaging-did-comm (#24)
    * Removing the example WASM implementation under affinidi-messaging-didcomm
    - this needs to be rewritten new from the SDK perspective
    
    * Delete affinidi-messaging-mediator/out
    
    ---------
    
    Co-authored-by: Robert <robert.k@affinidi.com>

--- affinidi-messaging-didcomm/wasm/src/did/mod.rs
+++
@@ -1,5 +0,0 @@
-mod did_doc;
-mod did_resolver;
-
-pub use did_resolver::DIDResolver;
-pub(crate) use did_resolver::JsDIDResolver;
--- affinidi-messaging-didcomm/wasm/tests-js/src/test-vectors/common.ts
+++
@@ -1,3 +0,0 @@
-export const ALICE_DID = "did:example:alice";
-export const BOB_DID = "did:example:bob";
-export const CHARLIE_DID = "did:example:charlie";
