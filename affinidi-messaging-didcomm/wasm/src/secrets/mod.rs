42c9156e1f9a059c9e4d7a048b044035b70e29d2
Glenn Gore <glenn.g@affinidi.com>
2024-09-13 16:39:42 +0800

    Removing the example WASM implementation under affinidi-messaging-didcomm - this needs to be rewritten new from the SDK perspective

--- affinidi-messaging-didcomm/wasm/src/secrets/mod.rs
+++
@@ -1,5 +0,0 @@
-mod secret;
-mod secrets_resolver;
-
-pub(crate) use secrets_resolver::JsSecretsResolver;
-pub use secrets_resolver::SecretsResolver;
--- affinidi-messaging-didcomm/wasm/tests-js/src/test-vectors/secrets/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./alice";
-export * from "./bob";
-export * from "./charlie";
-export * from "./charlie_rotated_to_alice";
