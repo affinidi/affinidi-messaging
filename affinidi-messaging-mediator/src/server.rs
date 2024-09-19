1d6b3d5d756522f002b18bab6ce3fed1c67fad11
YoussefAWasfy <youssef.w@affinidi.com>
2024-09-19 11:19:38 +0200

    feat: set everything to debug

--- affinidi-messaging-mediator/src/server.rs
+++ affinidi-messaging-mediator/src/server.rs
@@ -20,7 +20,7 @@ use tracing_subscriber::{
 
 pub async fn start() {
     // setup logging/tracing framework
-    let filter = filter::LevelFilter::INFO; // This can be changed in the config file!
+    let filter = filter::LevelFilter::DEBUG; // This can be changed in the config file!
     let (filter, reload_handle) = reload::Layer::new(filter);
     let ansi = env::var("LOCAL").is_ok();
     tracing_subscriber::registry()
@@ -141,8 +141,8 @@ pub async fn start() {
         )
         .layer(
             TraceLayer::new_for_http()
-                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
-                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::DEBUG))
+                .on_response(trace::DefaultOnResponse::new().level(Level::DEBUG)),
         )
         .layer(RequestBodyLimitLayer::new(config.http_size_limit as usize))
         // Add the healthcheck route after the tracing so we don't fill up logs with healthchecks
