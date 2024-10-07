a58945eea4b331cf3a901f296dc3c0e18ee0c41b
YoussefAWasfy <youssef.w@affinidi.com>
2024-10-07 14:03:43 +0200

    test: add integration tests to /outbound endpoint

--- affinidi-messaging-mediator/tests/integration_test.rs
+++ affinidi-messaging-mediator/tests/integration_test.rs
@@ -1,6 +1,6 @@
 use affinidi_did_resolver_cache_sdk::{config::ClientConfigBuilder, DIDCacheClient};
 use affinidi_messaging_didcomm::{
-    envelope::MetaEnvelope, secrets::SecretsResolver, AttachmentData, Message,
+    envelope::MetaEnvelope, secrets::SecretsResolver, Attachment, AttachmentData, Message,
     PackEncryptedOptions, UnpackMetadata, UnpackOptions,
 };
 use affinidi_messaging_mediator::{resolvers::affinidi_secrets::AffinidiSecrets, server::start};
@@ -10,7 +10,7 @@ use affinidi_messaging_sdk::{
     errors::ATMError,
     messages::{
         sending::InboundMessageResponse, AuthenticationChallenge, AuthorizationResponse,
-        GenericDataStruct, SuccessResponse,
+        GenericDataStruct, GetMessagesRequest, GetMessagesResponse, SuccessResponse,
     },
     protocols::{
         message_pickup::{MessagePickupDeliveryRequest, MessagePickupStatusReply},
@@ -29,6 +29,7 @@ use uuid::Uuid;
 
 const MY_DID: &str = "did:peer:2.Vz6MkgWJfVmPELozq6aCycK3CpxHN8Upphn3WSuQkWY6iqsjF.EzQ3shfb7vwQaTJqFkt8nRfo7Nu98tmeYpdDfWgrqQitDaqXRz";
 const MEDIATOR_API: &str = "https://localhost:7037/mediator/v1";
+const BOB_DID: &str = "did:peer:2.Vz6Mkihn2R3M8nY62EFJ7MAVXu7YxsTnuS5iAhmn3qKJbkdFf.EzQ3shpZRBUtewwzYiueXgDqs1bvGNkSyGoRgsbZJXt3TTb9jD.SeyJ0IjoiZG0iLCJzIjp7InVyaSI6Imh0dHBzOi8vbG9jYWxob3N0OjcwMzcvIiwiYWNjZXB0IjpbImRpZGNvbW0vdjIiXSwicm91dGluZ19rZXlzIjpbXX0sImlkIjpudWxsfQ";
 
 #[tokio::test]
 async fn test_mediator_server() {
@@ -45,7 +46,7 @@ async fn test_mediator_server() {
         .unwrap();
 
     // Signing and verification key
-    let v1 = json!({
+    let my_v1 = json!({
         "crv": "Ed25519",
         "d": "LLWCf83n8VsUYq31zlZRe0NNMCcn1N4Dh85dGpIqSFw",
         "kty": "OKP",
@@ -53,7 +54,7 @@ async fn test_mediator_server() {
     });
 
     // Encryption key
-    let e1 = json!({
+    let my_e1 = json!({
       "crv": "secp256k1",
       "d": "oi-dXG4EqfNODFPjv2vkieoLdbQZH9k6dwPDV8HDoms",
       "kty": "EC",
@@ -61,53 +62,72 @@ async fn test_mediator_server() {
       "y": "PpYqybOwMsm64vftt-7gBCQPIUbglMmyy_6rloSSAPk"
     });
 
+    let bob_v1 = json!({
+        "crv": "Ed25519",
+        "d": "FZMJijqdcp7PCQShgtFj6Ud3vjZY7jFZBVvahziaMMM",
+        "kty": "OKP",
+        "x": "PybG95kyeSfGRebp4T7hzA7JQuysc6mZ97nM2ety6Vo"
+    });
+    let bob_e1 = json!({
+        "crv": "secp256k1",
+        "d": "ai7B5fgT3pCBHec0I4Y1xXpSyrEHlTy0hivSlddWHZE",
+        "kty": "EC",
+        "x": "k2FhEi8WMxr4Ztr4u2xjKzDESqVnGg_WKrN1820wPeA",
+        "y": "fq0DnZ_duPWyeFK0k93bAzjNJVVHEjHFRlGOJXKDS18"
+    });
+
     let did_resolver = DIDCacheClient::new(ClientConfigBuilder::default().build())
         .await
         .unwrap();
-    let secrets_resolver = AffinidiSecrets::new(vec![
-        secret_from_str(&format!("{}#key-2", MY_DID), &e1),
-        secret_from_str(&format!("{}#key-1", MY_DID), &v1),
+    let my_secrets_resolver = AffinidiSecrets::new(vec![
+        secret_from_str(&format!("{}#key-1", MY_DID), &my_v1),
+        secret_from_str(&format!("{}#key-2", MY_DID), &my_e1),
+    ]);
+    let bob_secrets_resolver = AffinidiSecrets::new(vec![
+        secret_from_str(&format!("{}#key-1", BOB_DID), &bob_v1),
+        secret_from_str(&format!("{}#key-2", BOB_DID), &bob_e1),
     ]);
 
-    // Set up the HTTPS client
-    let mut client = ClientBuilder::new()
-        .use_rustls_tls()
-        .https_only(true)
-        .user_agent("Affinidi Trusted Messaging");
-
-    for cert in config.get_ssl_certificates() {
-        client =
-            client.add_root_certificate(Certificate::from_der(cert.to_vec().as_slice()).unwrap());
-    }
-
-    let client = match client.build() {
-        Ok(client) => client,
-        Err(e) => return assert!(false, "{:?}", e),
-    };
+    let client = init_client(config.clone());
 
     let mediator_did = _well_known(client.clone()).await;
 
     // Start Authentication
-    let authentication_challenge = _authenticate_challenge(client.clone()).await;
+    let my_authentication_challenge = _authenticate_challenge(client.clone(), MY_DID).await;
+    let bob_authentication_challenge = _authenticate_challenge(client.clone(), BOB_DID).await;
+    println!("Auth ch: {:#?}", my_authentication_challenge);
+    println!("Auth ch: {:#?}", bob_authentication_challenge);
+    // /authenticate/challenge
+    let my_auth_response_msg =
+        _create_auth_challenge_response(&my_authentication_challenge, MY_DID, &mediator_did);
 
-    let auth_response_msg = _create_auth_challenge_response(
-        &authentication_challenge,
-        MY_DID,
-        &mediator_did,
-        "authenticate",
-    );
-    let authentication_response = _authenticate(
+    let bob_auth_response_msg =
+        _create_auth_challenge_response(&bob_authentication_challenge, BOB_DID, &mediator_did);
+
+    // /authenticate
+    let my_authentication_response = _authenticate(
         client.clone(),
-        auth_response_msg,
+        my_auth_response_msg,
         MY_DID,
         &mediator_did,
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
+    )
+    .await;
+    let bob_authentication_response = _authenticate(
+        client.clone(),
+        bob_auth_response_msg,
+        BOB_DID,
+        &mediator_did,
+        &did_resolver,
+        &bob_secrets_resolver,
     )
     .await;
 
-    assert!(!authentication_response.access_token.is_empty());
-    assert!(!authentication_response.refresh_token.is_empty());
+    assert!(!my_authentication_response.access_token.is_empty());
+    assert!(!my_authentication_response.refresh_token.is_empty());
+    assert!(!bob_authentication_response.access_token.is_empty());
+    assert!(!bob_authentication_response.refresh_token.is_empty());
 
     // POST /inbound
     // MessageType=TrustPing
@@ -118,13 +138,13 @@ async fn test_mediator_server() {
         true,
         true,
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
     )
     .await;
 
     let signed_ping_res: SendMessageResponse<InboundMessageResponse> = _send_inbound_message(
         client.clone(),
-        authentication_response.clone(),
+        my_authentication_response.clone(),
         &signed_ping_msg,
         true,
         200,
@@ -154,12 +174,12 @@ async fn test_mediator_server() {
         false,
         false,
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
     )
     .await;
     let _anon_ping_res: SendMessageResponse<InboundMessageResponse> = _send_inbound_message(
         client.clone(),
-        authentication_response.clone(),
+        my_authentication_response.clone(),
         &anon_ping_msg,
         false,
         500,
@@ -171,30 +191,30 @@ async fn test_mediator_server() {
         &mediator_did,
         MY_DID.into(),
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
     )
     .await;
     let status_reply: SendMessageResponse<InboundMessageResponse> = _send_inbound_message(
         client.clone(),
-        authentication_response.clone(),
+        my_authentication_response.clone(),
         &status_request_msg,
         false,
         200,
     )
     .await;
-    _validate_status_reply(status_reply, &did_resolver, &secrets_resolver).await;
+    _validate_status_reply(status_reply, &did_resolver, &my_secrets_resolver).await;
 
     // MessageType=MessagePickupDeliveryRequest
     let delivery_request_msg = _build_delivery_request_message(
         &mediator_did,
         MY_DID.into(),
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
     )
     .await;
     let message_delivery: SendMessageResponse<InboundMessageResponse> = _send_inbound_message(
         client.clone(),
-        authentication_response.clone(),
+        my_authentication_response.clone(),
         &delivery_request_msg,
         true,
         200,
@@ -203,7 +223,7 @@ async fn test_mediator_server() {
     let message_received_ids = _validate_message_delivery(
         message_delivery,
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
         &pong_msg_id,
     )
     .await;
@@ -212,14 +232,14 @@ async fn test_mediator_server() {
     let message_received_msg = _build_message_received_message(
         &mediator_did,
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
         message_received_ids.clone(),
     )
     .await;
     let message_received_status_reply: SendMessageResponse<InboundMessageResponse> =
         _send_inbound_message(
             client.clone(),
-            authentication_response.clone(),
+            my_authentication_response.clone(),
             &message_received_msg,
             true,
             200,
@@ -228,14 +248,100 @@ async fn test_mediator_server() {
     _validate_message_received_status_reply(
         message_received_status_reply,
         &did_resolver,
-        &secrets_resolver,
+        &my_secrets_resolver,
     )
-    .await
+    .await;
+
+    // MessageType=ForwardRequest
+    let forward_request_msg = _build_forward_request_message(
+        &mediator_did,
+        MY_DID.into(),
+        BOB_DID.into(),
+        &did_resolver,
+        &bob_secrets_resolver,
+    )
+    .await;
+
+    let forward_request_response: SendMessageResponse<InboundMessageResponse> =
+        _send_inbound_message(
+            client.clone(),
+            my_authentication_response.clone(),
+            &forward_request_msg,
+            true,
+            200,
+        )
+        .await;
+
+    let forwarded_msg_id = _validate_forward_request_response(forward_request_response).await;
+    // /outbound
+    // delete messages: FALSE
+    let get_message_no_delete_request = GetMessagesRequest {
+        message_ids: vec![forwarded_msg_id.clone()],
+        delete: false,
+    };
+    let msg_list = _outbound_message(
+        client.clone(),
+        &get_message_no_delete_request,
+        my_authentication_response.clone(),
+        200,
+        false,
+    )
+    .await;
+
+    _validate_get_message_response(msg_list, MY_DID, &did_resolver, &my_secrets_resolver).await;
+
+    // delete messages: TRUE
+    let get_message_delete_request = GetMessagesRequest {
+        message_ids: vec![forwarded_msg_id.clone()],
+        delete: true,
+    };
+    let msg_list = _outbound_message(
+        client.clone(),
+        &get_message_delete_request,
+        my_authentication_response.clone(),
+        200,
+        false,
+    )
+    .await;
+
+    _validate_get_message_response(msg_list, MY_DID, &did_resolver, &my_secrets_resolver).await;
+
+    // get message should return not found
+    let msg_list = _outbound_message(
+        client.clone(),
+        &get_message_delete_request,
+        my_authentication_response.clone(),
+        200,
+        true,
+    )
+    .await;
 }
 
 async fn _start_mediator_server() {
     tokio::spawn(async move { start().await });
     println!("Server running");
+}
+
+fn init_client(config: Config<'_>) -> Client {
+    // Set up the HTTPS client
+    let mut client = ClientBuilder::new()
+        .use_rustls_tls()
+        .https_only(true)
+        .user_agent("Affinidi Trusted Messaging");
+
+    for cert in config.get_ssl_certificates() {
+        client =
+            client.add_root_certificate(Certificate::from_der(cert.to_vec().as_slice()).unwrap());
+    }
+
+    let client = match client.build() {
+        Ok(client) => client,
+        Err(e) => {
+            assert!(false, "{:?}", e);
+            panic!();
+        }
+    };
+    client
 }
 
 async fn _well_known(client: Client) -> String {
@@ -266,11 +372,11 @@ async fn _well_known(client: Client) -> String {
     did
 }
 
-async fn _authenticate_challenge(client: Client) -> AuthenticationChallenge {
+async fn _authenticate_challenge(client: Client, did: &str) -> AuthenticationChallenge {
     let res = client
         .post(format!("{}/authenticate/challenge", MEDIATOR_API))
         .header("Content-Type", "application/json")
-        .body(format!("{{\"did\": \"{}\"}}", MY_DID).to_string())
+        .body(format!("{{\"did\": \"{}\"}}", did).to_string())
         .send()
         .await
         .unwrap();
@@ -341,10 +447,11 @@ async fn _authenticate<'sr>(
         .unwrap();
 
     let status = res.status();
-    assert!(status.is_success());
     println!("Authentication response: status({})", status);
 
     let body = res.text().await.unwrap();
+
+    assert!(status.is_success());
 
     if !status.is_success() {
         println!("Failed to get authentication response. Body: {:?}", body);
@@ -363,16 +470,12 @@ fn _create_auth_challenge_response(
     body: &AuthenticationChallenge,
     my_did: &str,
     atm_did: &str,
-    protocol_message: &str,
 ) -> Message {
-    let now = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
+    let now = _get_time_now();
 
     Message::build(
         Uuid::new_v4().into(),
-        format!("https://affinidi.com/atm/1.0/{}", protocol_message),
+        "https://affinidi.com/atm/1.0/authenticate".to_owned(),
         json!(body),
     )
     .to(atm_did.to_owned())
@@ -519,7 +622,6 @@ async fn _build_delivery_request_message<'sr>(
     msg = msg.from(MY_DID.to_owned());
     let now = _get_time_now();
     let msg = msg.created_time(now).expires_time(now + 300).finalize();
-    let msg_id = msg.id.clone();
 
     // Pack the message
     let (msg, _) = msg
@@ -698,11 +800,100 @@ async fn _validate_message_received_status_reply<S>(
         .unwrap();
         let status: MessagePickupStatusReply =
             serde_json::from_value(message.body.clone()).unwrap();
+
         assert!(!status.live_delivery);
         assert!(status.longest_waited_seconds.is_none());
         assert!(status.message_count == 0);
         assert!(status.recipient_did == MY_DID);
         assert!(status.total_bytes == 0);
+    }
+}
+
+async fn _build_forward_request_message<'sr>(
+    mediator_did: &str,
+    recipient_did: String,
+    actor_did: String,
+    did_resolver: &DIDCacheClient,
+    secrets_resolver: &'sr (dyn SecretsResolver + 'sr + Sync),
+) -> String {
+    let now = _get_time_now();
+
+    let recipient_did = recipient_did;
+
+    let msg = Message::build(
+        Uuid::new_v4().into(),
+        "https://didcomm.org/routing/2.0/forward".to_owned(),
+        json!({ "next": recipient_did }),
+    )
+    .to(mediator_did.to_owned())
+    .from(actor_did.clone())
+    .attachment(
+        Attachment::json(json!({ "message": "plaintext attachment, mediator can read this" }))
+            .finalize(),
+    )
+    .attachment(
+        Attachment::base64(String::from(
+            "ciphertext and iv which is encrypted by the recipient public key",
+        ))
+        .finalize(),
+    );
+
+    let msg = msg.created_time(now).expires_time(now + 300).finalize();
+
+    // Pack the message
+    let (msg, _) = msg
+        .pack_encrypted(
+            &mediator_did.to_owned(),
+            Some(&actor_did.clone()),
+            Some(&actor_did.clone()),
+            &did_resolver,
+            secrets_resolver,
+            &PackEncryptedOptions::default(),
+        )
+        .await
+        .unwrap();
+    msg
+}
+
+async fn _validate_forward_request_response(
+    forward_request_response: SendMessageResponse<InboundMessageResponse>,
+) -> String {
+    let msg_id = if let SendMessageResponse::RestAPI(Some(InboundMessageResponse::Stored(m))) =
+        forward_request_response
+    {
+        if let Some((_, msg_id)) = m.messages.first() {
+            Some(msg_id.to_owned())
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    assert!(!msg_id.is_none());
+
+    msg_id.unwrap()
+}
+
+async fn _validate_get_message_response<S>(
+    list: GetMessagesResponse,
+    my_did: &str,
+    did_resolver: &DIDCacheClient,
+    secrets_resolver: &S,
+) where
+    S: SecretsResolver + Send,
+{
+    for msg in list.success {
+        assert_eq!(msg.to_address.unwrap(), digest(my_did));
+        let _ = Message::unpack_string(
+            &msg.msg.unwrap(),
+            did_resolver,
+            secrets_resolver,
+            &UnpackOptions::default(),
+        )
+        .await
+        .unwrap();
+        println!("Msg id: {}", msg.msg_id);
     }
 }
 
@@ -741,6 +932,69 @@ where
     };
 
     SendMessageResponse::RestAPI(http_response)
+}
+
+async fn _outbound_message(
+    client: Client,
+    messages: &GetMessagesRequest,
+    tokens: AuthorizationResponse,
+    expected_status_code: u16,
+    expecting_get_errors: bool,
+) -> GetMessagesResponse {
+    let body = serde_json::to_string(messages)
+        .map_err(|e| {
+            ATMError::TransportError(format!("Could not serialize get message request: {:?}", e))
+        })
+        .unwrap();
+
+    let res = client
+        .post(format!("{}/outbound", MEDIATOR_API))
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {}", tokens.access_token))
+        .body(body)
+        .send()
+        .await
+        .map_err(|e| {
+            ATMError::TransportError(format!("Could not send get_messages request: {:?}", e))
+        })
+        .unwrap();
+
+    let status = res.status();
+    println!("API response: status({})", status);
+    assert_eq!(status, expected_status_code);
+    let body = res
+        .text()
+        .await
+        .map_err(|e| ATMError::TransportError(format!("Couldn't get body: {:?}", e)))
+        .unwrap();
+
+    let body = serde_json::from_str::<SuccessResponse<GetMessagesResponse>>(&body)
+        .ok()
+        .unwrap();
+
+    let list = if let Some(list) = body.data {
+        list
+    } else {
+        panic!("No messages found")
+    };
+
+    if expecting_get_errors {
+        assert!(!list.get_errors.is_empty())
+    } else {
+        assert!(list.get_errors.is_empty())
+    }
+
+    if !list.get_errors.is_empty() {
+        for (msg, err) in &list.get_errors {
+            println!("failed get: msg({}) error({})", msg, err);
+        }
+    }
+    if !list.delete_errors.is_empty() {
+        for (msg, err) in &list.delete_errors {
+            println!("failed delete: msg({}) error({})", msg, err);
+        }
+    }
+    list
 }
 
 fn _get_time_now() -> u64 {
