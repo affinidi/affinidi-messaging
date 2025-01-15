010c5310ab866fc9946a2c1fe3df2e19c68af0ab
Ersin Ihsan Unkar <eiunkar@gmail.com>
2025-01-15 15:43:46 +0100

    feat: change loogging to json

--- affinidi-messaging-mediator/src/tasks/statistics.rs
+++ affinidi-messaging-mediator/src/tasks/statistics.rs
@@ -21,8 +21,34 @@ pub async fn statistics(database: DatabaseHandler) -> Result<(), MediatorError> 
         loop {
             interval.tick().await;
             let stats = database.get_db_metadata().await?;
-            info!("Statistics: {}", stats);
-            info!("Delta: {}", stats.delta(&previous_stats));
+            let delta = stats.delta(&previous_stats);
+            info!(event_type="UpdateStats",
+                  received_bytes=stats.received_bytes,
+                  sent_bytes=stats.sent_bytes,
+                  deleted_bytes=stats.deleted_bytes,
+                  received_count=stats.received_count,
+                  sent_count=stats.sent_count,
+                  deleted_count=stats.deleted_count,
+                  websocket_open=stats.websocket_open,
+                  websocket_close=stats.websocket_close,
+                  sessions_created=stats.sessions_created,
+                  sessions_success=stats.sessions_success,
+                  oob_invites_created=stats.oob_invites_created,
+                  oob_invites_claimed=stats.oob_invites_claimed);
+
+            info!(event_type="UpdateDeltaStats",
+                  received_bytes=delta.received_bytes,
+                  sent_bytes=delta.sent_bytes,
+                  deleted_bytes=delta.deleted_bytes,
+                  received_count=delta.received_count,
+                  sent_count=delta.sent_count,
+                  deleted_count=delta.deleted_count,
+                  websocket_open=delta.websocket_open,
+                  websocket_close=delta.websocket_close,
+                  sessions_created=delta.sessions_created,
+                  sessions_success=delta.sessions_success,
+                  oob_invites_created=delta.oob_invites_created,
+                  oob_invites_claimed=delta.oob_invites_claimed);
 
             previous_stats = stats;
         }
